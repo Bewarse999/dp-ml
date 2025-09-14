694526c948ecc000096bd4b36365ff6e0089a787
Mohan Manoj <143806882+Bewarse999@users.noreply.github.com>
2025-09-14 15:23:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
-st.title('HeartDisease')
-
-st.write('Hello world!')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+# loading the csv data to a Pandas DataFrame
+heart_data = pd.read_csv('/content/heart.csv')
